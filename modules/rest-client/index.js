00c1783908b1e7facfb80626fa372dab40e72373
Chris Thierer <cthierer1@gmail.com>
2016-01-31 14:20:55 -0500

    Add index lookups for REST client

---
+++ modules/rest-client/index.js
@@ -0,0 +1,27 @@
+/**
+ * REST Client module 
+ * @module rest-client
+ */
+
+module.exports = {
+
+    /**
+     * @see module:rest-client/errors
+     */
+    errors: require('./errors'),
+
+    /**
+     * @see module:rest-client/models
+     */
+    models: require('./models'),
+
+    /**
+     * @see module:rest-client/services
+     */
+    services: require('./services'),
+
+    /**
+     * @see module:rest-client/utils
+     */
+    utils: require('./utils')
+};
---
+++ modules/rest-client/services/index.js
@@ -0,0 +1,12 @@
+/**
+ * Services 
+ * @module rest-client/services 
+ */ 
+
+module.exports = {
+
+    /**
+     * @see module:rest-client/services/RESTClientService
+     */
+    RESTClientService: require('./rest_client_service')
+};
